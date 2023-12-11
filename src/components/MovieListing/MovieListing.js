c6a7b8e679718bad3a56c4e74332fc9e00bd71b6
Saadat23-4 <saadat.nurbek.ch@gmail.com>
2023-12-11 14:00:15 -0600

    Update Movie-app submodule

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Slider from "react-slick";
+import { Settings } from "../../common/settings";
 import { useSelector } from "react-redux";
 import { getAllMovies, getAllShows } from "../../features/movies/movieSlice";
 import { MovieCard } from "../MovieCard/MovieCard";
@@ -13,7 +15,7 @@ export const MovieListing = () => {
   renderMovies =
     movies.Response === "True" ? (
       movies.Search.map((movie, index) => {
-        return <MovieCard key={index} data={movie} />;
+        return <MovieCard key={movie.imdbID} data={movie} />;
       })
     ) : (
       <div className="movies-error">
@@ -25,7 +27,7 @@ export const MovieListing = () => {
   renderShows =
     shows.Response === "True" ? (
       shows.Search.map((show, index) => {
-        return <MovieCard key={index} data={show} />;
+        return <MovieCard key={show.imdbID} data={show} />;
       })
     ) : (
       <div className="movies-error">
@@ -37,11 +39,15 @@ export const MovieListing = () => {
     <div className="movie-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
-        <div className="movie-container">{renderMovies}</div>
+        <div className="movie-container">
+          <Slider {...Settings}>{renderMovies}</Slider>
+        </div>
       </div>
       <div className="show-list">
         <h2>Shows</h2>
-        <div className="movie-container">{renderShows}</div>
+        <div className="movie-container">
+          <Slider {...Settings}>{renderShows}</Slider>
+        </div>
       </div>
     </div>
   );
